5a5ac06e566c3cbe56b2b5a834e2b62bf63d85a1
이현정 <h.jung0409@daum.net>
2021-08-14 19:05:16 +0900

    범규

---
+++ baekjoon.problems/src/BOJ_15657_N과M8.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//중복조합
+public class BOJ_15657_N과M8 {
+	static int N, M, numbers[], combination[];
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		N =Integer.parseInt(st.nextToken());
+		M =Integer.parseInt(st.nextToken());
+		
+		numbers = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(numbers);
+		
+		combination = new int[M];
+		makeCombination(0,0);
+		System.out.println(sb.toString());
+	}//main close
+
+	private static void makeCombination(int cnt, int start) {
+		if(cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(combination[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int idx = start; idx < N; idx++) {
+			combination[cnt]= numbers[idx];
+			makeCombination(cnt+1, idx);
+		}
+		
+	}//combination close
+		
+}
