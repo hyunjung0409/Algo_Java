1d7327644909a26a25b68eef7033e6ff661521f5
이현정 <h.jung0409@daum.net>
2021-08-30 23:35:04 +0900

    Solving a problem with String

---
+++ baekjoon.problems/src/BOJ_10809.java
@@ -0,0 +1,31 @@
+// BOJ_10809_알파벳 찾기
+// https://www.acmicpc.net/problem/10809
+
+import java.util.Scanner;
+
+public class BOJ_10809 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[26];
+		for (int i = 0; i < 26; i++) {
+			arr[i] = -1;
+		}
+		
+		// a --> 97
+		String str = sc.next();
+		for (int i = 0; i < str.length(); i++) {
+			int idx = str.charAt(i) - 97;
+			if(arr[idx] == -1)
+				arr[idx] = i;
+			else
+				continue;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 26; i++) {
+			sb.append(arr[i]+" ");
+		}
+		System.out.println(sb.toString());
+	}
+}
