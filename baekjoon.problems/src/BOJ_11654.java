318c950e1d891d70f48a3b5bac1020504c1a48c1
이현정 <h.jung0409@daum.net>
2021-08-30 23:04:39 +0900

    solving a problem

---
+++ baekjoon.problems/src/BOJ_11654.java
@@ -0,0 +1,12 @@
+// BOJ_11654_아스키코드
+// https://www.acmicpc.net/problem/11654
+import java.util.Scanner;
+
+public class BOJ_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.next().charAt(0);
+		System.out.println(input);
+	}
+}
