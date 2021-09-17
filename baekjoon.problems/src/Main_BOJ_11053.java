492a383168c42a452b648d9cd626885a56220c45
이현정 <h.jung0409@daum.net>
2021-09-17 11:30:51 +0900

    BOJ_1105_가장 긴 증가하는 부분 수열_DP

--- baekjoon.problems/src/Main_BOJ_11053.java
+++ baekjoon.problems/src/Main_BOJ_11053.java
@@ -12,29 +12,27 @@ public class Main_BOJ_11053 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//n이 1000 이하로 DP로 풀이 (n제곱)
 		int n = Integer.parseInt(br.readLine());
 		int[] num = new int[n];
 		int[] LIS = new int[n];
-
+		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < n; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
-
-		LIS[0] = num[0];
-		int size = 1;
-		for (int i = 1; i < n; i++) {
-			if (LIS[size - 1] < num[i]) // 이전 값보다 큰 값이 들어오면
-				LIS[size++] = num[i]; // LIS 배열에 붙인다
-			else if (LIS[0] > num[i])
-				LIS[0] = num[i]; // 최소값으로 갱신
-			else {
-				int temp =Arrays.binarySearch(LIS, 0, size, num[i]);
-				LIS[temp<0? Math.abs(temp)-1 : temp] = num[i];
-			}
+		
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			LIS[i] = 1; //자기 자신을 놓는 최소값(1)로 초기화
+			for (int j = 0; j < i; j++) { //j: i의 앞쪽 원소
+				//자기 자신(i)가 더 크고, 앞쪽 원소 길이+1 보다 짧을 때
+				if(num[i]>num[j] && LIS[i]<LIS[j]+1)
+					LIS[i] = LIS[j]+1;
+			}//i를 끝으로 하는 최장 길이 계산 완료
+			//LIS[i]: i를 끝으로 하는 최장 증가 수열의 길이
+			if(max<LIS[i]) max = LIS[i];
 		}
-
-		System.out.println(size);
+		System.out.println(max);
 	}
-
 }
