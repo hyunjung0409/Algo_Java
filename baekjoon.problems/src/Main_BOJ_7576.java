b342b3a3f6a2bb98c0966fda9bd55ba9118896b0
이현정 <h.jung0409@daum.net>
2021-10-02 00:37:53 +0900

    BOJ_7576_토마토_BFS

--- baekjoon.problems/src/Main_BOJ_7576.java
+++ baekjoon.problems/src/Main_BOJ_7576.java
@@ -38,15 +38,15 @@ public class Main_BOJ_7576 {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 1) {
+				if (map[i][j] == 1) { //익은 토마토면 list에 추가
 					list.add(new Point(i, j, 0));
-				} else if (map[i][j] == 0) {
+				} else if (map[i][j] == 0) { //익지 않은 토마토면 count
 					green++;
 				}
 			}
 		}
-		if (green == 0)
-			System.out.println(0);
+		if (green == 0) //익지 않은 토마토가 없으면
+			System.out.println(0); //0 출력
 		else
 			System.out.println(bfs());
 
@@ -54,10 +54,11 @@ public class Main_BOJ_7576 {
 
 	private static int bfs() {
 		Queue<Point> q = new LinkedList<>();
+		//익은 토마토 개수만큼 start 객체를 생성
 		for (int i = 0; i < list.size(); i++) {
 			int r = list.get(i).r;
 			int c = list.get(i).c;
-			Point start = new Point(r, c, 1);
+			Point start = new Point(r, c, 1); 
 			visited[r][c] = true;
 			q.offer(start);
 		}
@@ -68,20 +69,26 @@ public class Main_BOJ_7576 {
 			int cc = cur.c;
 			int cd = cur.day;
 
-			for (int d = 0; d < 4; d++) {
+			for (int d = 0; d < 4; d++) { //사방탐색
+				//이동할 좌표
 				int nr = cr + dr[d];
 				int nc = cc + dc[d];
 
 				if (check(nr, nc) && !visited[nr][nc]) {
+					//토마토가 들어있지 않은 칸일 경우
 					if (map[nr][nc] == -1)
 						continue;
+					//익지 않은 토마토가 있는 칸일 경우
 					else if (map[nr][nc] == 0) {
 						visited[nr][nc] = true;
-						map[nr][nc] = 1;
-						green -= 1;
+						map[nr][nc] = 1; //익은 토마토 처리
+						green -= 1; //익지 않은 토마토 수 -1
+						//토마토가 다 익은 경우
 						if (green == 0) {
+							//익기까지의 기간
 							return cd;
 						}
+						//이동할 좌표, 날짜+1 해서 큐에 넣어준다
 						q.offer(new Point(nr, nc, cd + 1));
 					}
 				}
